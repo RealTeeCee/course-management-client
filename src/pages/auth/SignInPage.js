4f36b770b166320721431c2eda20a6990b20ffed
trucduy10 <trucduy.truong@gmail.com>
2023-05-01 11:09:13 +0700

    Draft Sign in

--- src/pages/auth/SignInPage.js
+++ src/pages/auth/SignInPage.js
@@ -5,7 +5,7 @@ const SignInPage = () => {
     return (
         <LayoutAuthentication>
             Sign-in
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere cumque cum incidunt minus iusto reprehenderit, commodi, consequuntur quidem molestias debitis iure dolorem quibusdam voluptates recusandae velit odio ad atque tempora.
+            
         </LayoutAuthentication>
     );
 };
