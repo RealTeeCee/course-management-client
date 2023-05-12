7b9a605ffac46757cdddc4a4b750c7155092de5e
trucduy10 <trucduy.truong@gmail.com>
2023-05-12 19:42:40 +0700

    update assest & blog

---
+++ src/pages/blog/BlogSelectPage.js
@@ -0,0 +1,99 @@
+import React from "react";
+import { HeadingFormH1Com, HeadingH2Com } from "../../components/heading";
+import { BsFillPersonVcardFill,BsArrowLeftSquareFill,BsArrowRightSquareFill } from "react-icons/bs";
+import { AiOutlineTags, AiOutlineClockCircle } from "react-icons/ai";
+import { blog } from "../../assets/blog_data/data";
+import { Link } from "react-router-dom";
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import Stack from '@mui/material/Stack';
+
+const BlogSelectPage = () => {
+  return (
+    <>
+      <div className="max-w-[1240px] mx-auto py-6 px-4 text-center">
+        <HeadingFormH1Com>MY BLOG </HeadingFormH1Com>
+        <h2 className="py-4">
+          <div>
+            We’ve got everything you need to deliver flexible and effective
+            skills development for your entire workforce.
+          </div>
+          <div>
+            Teach what you know and help learners explore their interests, gain
+            new skills, and advance their careers.
+          </div>
+          <div>
+            Publish the course you want, in the way you want, and always have
+            control of your own content.
+          </div>
+          <div>
+            Expand your professional network, build your expertise, and earn money on each paid enrollment.
+          </div>
+        </h2>
+      </div>
+      <section className="my-12">
+        <div className="max-w-[95%] m-auto grid lg:grid-cols-3 sm:grid-cols-2 gap-4">
+          {blog.map((blog) => (
+            <div
+              key={blog.id}
+              className="transition-all duration-[0.5s] border-solid border-[1px] border-[#e6e6e6] rounded-[12px] p-[20px] bg-white hover:shadow-[0_2px_4px_rgb(0_0_0_/_8%)] hover:cursor-pointer hover:translate-y-[-5px]"
+            >
+              <div id="img">
+                <img
+                  src={require("../../assets/blog_image/" + blog.cover)}
+                  alt=""
+                  className="w-full h-[250px] object-cover rounded-[10px] mb-[20px]"
+                />
+              </div>
+              <div className="flex items-center mb-3">
+              <AiOutlineTags className="mr-[10px] text-[25px]" />
+                  <label className="block mr-[20px] mb-0 text-[#999] text-[15px]">
+                    {blog.category}
+                  </label>
+              </div>
+              
+              <div id="details">
+                <Link to={`/details/${blog.id}`}>
+                  <div className="text-black border-none bg-none outline-none cursor-pointer no-underline list-none text-[17px]">
+                    <h3 className="font-[500]">{blog.title}</h3>
+                  </div>
+                </Link>
+                <p className="text-[#999] font-[400] my-[20px] text-[17px] leading-[25px]">
+                  {blog.desc}...
+                </p>
+                <div id="date" className="flex items-center mt-3">
+                  
+                  <AiOutlineClockCircle className="mr-[10px] text-[40px]" />
+                  <label className="block mr-[20px] mb-0 text-[#999] text-[13px]">
+                    {blog.date}
+                  </label>
+                  <BsFillPersonVcardFill className="mr-[10px] text-[40px]" />
+                  <label className="block mr-[20px] mb-0 text-[#999] text-[13px]">
+                    {blog.author}
+                  </label>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+        <Stack spacing={2} >
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Pagination 
+            count={10}
+            renderItem={(item) => (
+              <PaginationItem
+                slots={{ previous:  BsArrowLeftSquareFill , next: BsArrowRightSquareFill }}
+                {...item}
+              />
+            )}
+          />
+          </div>
+        </Stack>
+
+    </>
+  );
+};
+
+export default BlogSelectPage;
---
+++ src/pages/blog/DetailsPage.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { BsPencilSquare } from "react-icons/bs";
+import { AiOutlineDelete } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { blog } from "../../assets/blog_data/data";
+import LayoutHome from "../../layouts/LayoutHome";
+import { HeadingH2Com } from "../../components/heading";
+
+const DetailsPage = () => {
+  const { id } = useParams();
+  const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    let blogs = blog.find((blogs) => blogs.id === parseInt(id));
+    if (blogs) {
+      setBlogs(blogs);
+    }
+  }, []);
+
+  return (
+       <LayoutHome>
+      {blogs ? (
+        <section className=" bg-white max-w-[1240px] mx-auto py-10 px-24 ">
+          <div className="container">
+            <div className="flex justify-center">
+              <img
+                className="w-full h-full rounded-2xl"
+                src={require("../../assets/blog_image/" + blogs.cover)}
+                alt=""
+              />
+            </div>
+            <div className="mt-8">
+              <div className="flex justify-end">
+                <button className="ml-5 border-none">
+                  <BsPencilSquare />
+                </button>
+                <button className="ml-5 border-none">
+                  <AiOutlineDelete />
+                </button>
+              </div>
+              <h1 className="text-3xl font-medium">{blogs.title}</h1>
+              <HeadingH2Com className="my-5 text-lg capitalize leading-7">{blogs.desc}</HeadingH2Com>
+              <p className="my-5 text-lg capitalize leading-7">
+                "But I must explain to you how all this mistaken idea of
+                denouncing pleasure and praising pain was born and I will give
+                you a complete account of the system, and expound the actual
+                teachings of the great explorer of the truth, the master-builder
+                of human happiness. No one rejects, dislikes, or avoids pleasure
+                itself, because it is pleasure, but because those who do not
+                know how to pursue pleasure rationally encounter consequences
+                that are extremely painful. Nor again is there anyone who loves
+                or pursues or desires to obtain pain of itself, because it is
+                pain, but because occasionally circumstances occur in which toil
+                and pain can procure him some great pleasure. To take a trivial
+                example, which of us ever undertakes laborious physical
+                exercise, except to obtain some advantage from it? But who has
+                any right to find fault with a man who chooses to enjoy a
+                pleasure that has no annoying consequences, or one who avoids a
+                pain that produces no resultant pleasure?" Section 1.10.33 of
+                "de Finibus Bonorum et Malorum", written by Cicero in 45 BC "At
+                vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas molestias excepturi sint occaecati cupiditate
+                non provident, similique sunt in culpa qui officia deserunt
+                mollitia animi, id est laborum et dolorum fuga. Et harum quidem
+                rerum facilis est et expedita distinctio. Nam libero tempore,
+                cum soluta nobis est eligendi optio cumque nihil impedit quo
+                minus id quod maxime placeat facere possimus, omnis voluptas
+                assumenda est, omnis dolor repellendus. Temporibus autem
+                quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+                eveniet ut et voluptates repudiandae sint et molestiae non
+                recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
+                ut aut reiciendis voluptatibus maiores alias consequatur aut
+                perferendis doloribus asperiores repellat."
+              </p>
+              <p className="my-5 text-lg capitalize leading-7">
+                Author: {blogs.author}
+              </p>
+            </div>
+          </div>
+        </section>
+      ) : null}
+    </LayoutHome>
+  );
+};
+
+export default DetailsPage;
