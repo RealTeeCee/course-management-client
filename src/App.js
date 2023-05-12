4207890cebfd6dbc65abf4e211a55175ac44a3a8
ricpham <ricpham1902@gmail.com>
2023-05-12 12:44:39 +0700

    Add CourseDetailPage

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import LayoutHome from "./layouts/LayoutHome.js";
 
 const RegisterPage = lazy(() => import("./pages/auth/RegisterPage.js"));
 const LoginPage = lazy(() => import("./pages/auth/LoginPage.js"));
+const AdminPage = lazy(() => import("./pages/admin/AdminPage.js"));
 
 const HomePage = lazy(() => import("./pages/HomePage.js"));
+
 const CoursePage = lazy(() => import("./pages/course/CoursePage.js"));
 const MyCoursePage = lazy(() => import("./pages/course/MyCoursePage.js"));
-const AdminPage = lazy(() => import("./pages/admin/AdminPage.js"));
+const CourseDetailPage = lazy(() =>
+  import("./pages/course/CourseDetailPage.js")
+);
 const CreateCoursePage = lazy(() =>
   import("./pages/course/CreateCoursePage.js")
 );
@@ -22,6 +26,10 @@ function App() {
         <Route element={<LayoutHome></LayoutHome>}>
           <Route path="/" element={<HomePage></HomePage>}></Route>
           <Route path="/courses" element={<CoursePage></CoursePage>}></Route>
+          <Route
+            path="/courses/:slug"
+            element={<CourseDetailPage></CourseDetailPage>}
+          ></Route>
           <Route
             path="/my-courses"
             element={<MyCoursePage></MyCoursePage>}
--- src/pages/course/index.js
+++
@@ -1,4 +0,0 @@
-export const { default: CoursePage } = require("./CoursePage");
-export const { default: MyCoursePage } = require("./MyCoursePage");
-export const { default: CreateCoursePage } = require("./CreateCoursePage");
-
