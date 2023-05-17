62a88705ec70bbe7f1e53a722581efc97b2c7313
trucduy10 <trucduy.truong@gmail.com>
2023-05-17 17:43:39 +0700

    update blog with paginationBlog 2nd

--- src/hooks/usePaginationBlog.js
+++ src/hooks/usePaginationBlog.js
@@ -1,22 +1,22 @@
 import { useState } from "react";
-
 export default function usePaginationBlog(
-  initialPage = 1,
-  defaultPageSize = 6
-) {
-  const [currentPage, setCurrentPage] = useState(initialPage);
-const limit = defaultPageSize;
-  const startOffSet = (currentPage - 1) * parseInt(defaultPageSize);
-  const endOffSet = startOffSet + parseInt(defaultPageSize);
-
-  const handleChangePage = (page) => {
-    setCurrentPage(page);
-  };
-  return {
-    currentPage,
-    handleChangePage,
-    startOffSet,
-    endOffSet,
-    limit
-  };
-}
+    initialPage = 1,
+    defaultPageSize = 6
+){
+    const [currentPage, setCurrentPage] = useState(initialPage);
+  const limit = defaultPageSize ;
+    const startOffSet =
+      (currentPage - 1) * parseInt(defaultPageSize);
+    const endOffSet = startOffSet + parseInt(defaultPageSize);
+  
+    const handleChangePage = (page) => {
+      setCurrentPage(page);
+    };
+    return {
+      currentPage,
+      handleChangePage,
+      startOffSet,
+      endOffSet,
+      limit
+    }; 
+  }
