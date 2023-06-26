4148b2acd872ce8e1f1faec9d42e9aa95998e403
NguyenNguyenTally <124548339+NguyenNguyenTally@users.noreply.github.com>
2023-06-26 20:41:25 +0700

    Exam UI

---
+++ src/pages/exam/ExamPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Quiz from "../../components/mui/Quiz";
+import { Container } from "@mui/material";
+
+const ExamPage = () => {
+  return (
+    <Container maxWidth="sm">
+      <Quiz />
+    </Container>
+  );
+};
+
+export default ExamPage;
