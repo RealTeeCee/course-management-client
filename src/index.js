e294dd3e036553fcf1ad6fce6efccc25084164b4
ricpham <ricpham1902@gmail.com>
2023-07-11 22:26:38 +0700

    Clean source P1

--- src/index.js
+++ src/index.js
@@ -5,26 +5,20 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/configureStore";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { ToastContainer } from "react-toastify";
+import { ConfigProvider } from "antd";
+
 import "react-toastify/dist/ReactToastify.css";
 
-import "./assets/css/font-awesome.css";
 import "./assets/css/vendors/icofont.css";
 import "./assets/css/vendors/themify.css";
-import "./assets/css/vendors/flag-icon.css";
-import "./assets/css/vendors/feather-icon.css";
-import "./assets/css/vendors/scrollbar.css";
-import "./assets/css/vendors/animate.css";
-import "./assets/css/vendors/chartist.css";
-import "./assets/css/vendors/date-picker.css";
 import "./assets/css/vendors/bootstrap.css";
 import "./assets/css/style.css";
 import "./assets/css/color-1.css";
 import "./assets/css/responsive.css";
-import { PersistGate } from "redux-persist/integration/react";
 import "./index.scss";
 import "swiper/css";
-import { ToastContainer } from "react-toastify";
-import { ConfigProvider } from "antd";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
