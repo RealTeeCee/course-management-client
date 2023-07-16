d5f6aec11b5f142a58e3409bdcced38023ec2106
trucduy10 <trucduy.truong@gmail.com>
2023-07-16 19:30:47 +0700

    Copyright in footer

--- src/modules/footer/FooterMod.js
+++ src/modules/footer/FooterMod.js
@@ -9,17 +9,18 @@ const FooterMod = () => {
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-3 pt-3 px-28">
             <img
-              // className="h-14 mb-4"
               className=" w-[200px] object-cover"
               src="/logo_click_light.png"
               alt="Click and Learn Logo"
             />
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-3 pb-10 px-28">
+          <div className="grid grid-cols-1 md:grid-cols-3 px-28 pb-3">
             {/* Column 1 */}
             <div className="md:col-span-1 px-4">
               <h5 className="font-bold mb-4 text-indigo-600">CONTACT US</h5>
-              <p className="text-sm mb-2">Phone: (08)38460801</p>
+              <p className="text-sm mb-2 transition-transform hover:scale-100 ">
+                Phone: (08)38460801
+              </p>
               <p className="text-sm mb-2">Email: contact@clickandlearn.com</p>
               <p className="text-sm ">
                 590 Cach Mang Thang 8, District 3, Ho Chi Minh City
@@ -43,7 +44,7 @@ const FooterMod = () => {
                   <li className="mb-2">
                     <a href="/courses">Courses Page</a>
                   </li>
-                
+
                   <li className="mb-2">
                     <a href="/categories">Categories Page</a>
                   </li>
@@ -54,16 +55,22 @@ const FooterMod = () => {
             {/* Column 3 */}
             <div className="md:col-span-1 px-4 flex justify-center">
               <div>
-              <h5 className="font-bold mb-4 text-indigo-600">DOWNLOAD NOW</h5>
-              <a href="/click_and_learn_app.apk" download>
-                <img
-                  className="w-[150px] object-cover transition-transform transform-gpu hover:scale-110 hover:brightness-50"
-                  src="/footer_download_android.png"
-                  alt="Download Android"
-                />
-              </a>
+                <h5 className="font-bold mb-4 text-indigo-600">DOWNLOAD NOW</h5>
+                <a href="/click_and_learn_app.apk" download>
+                  <img
+                    className="w-[150px] object-cover transition-transform transform-gpu hover:scale-110 hover:brightness-50"
+                    src="/footer_download_android.png"
+                    alt="Download Android"
+                  />
+                </a>
               </div>
-             
+            </div>
+          </div>
+          <hr className="w-150 mt-2 border-b-2 border-white" />
+          <div className=" pt-4 pb-10 px-28 text-center">
+           
+            <div className="flex items-center justify-center">
+              <p className="text-sm mb-2">Copyright © 2023 Click & Learn</p>
             </div>
           </div>
         </div>
